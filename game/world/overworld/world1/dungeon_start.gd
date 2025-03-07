f8d98a96ae0caa4ac2bc30d8c81c96930087e924
Aksel <aknea001@osloskolen.no>
2025-03-07 13:33:58 +0100

    can enter castle
    gotta add a "throbbing" effect on the label or smth that moves yk yk

---
+++ game/world/overworld/world1/dungeon_start.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var readyToGo = false
+
+@onready var enterLabel = $enterLabel
+
+func _on_body_entered(_body: Node2D) -> void:
+	readyToGo = true
+
+func _on_body_exited(_body: Node2D) -> void:
+	readyToGo = false
+	enterLabel.visible = false
+
+func _process(_delta: float) -> void:
+	if readyToGo:
+		enterLabel.visible = true
+		print(lerp(50, 60, 10))
+		if Input.is_action_just_pressed("enter"):
+			get_tree().change_scene_to_file("res://game/world/overworld/test_world/dungeonTypeShit.tscn")
