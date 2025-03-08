53ab9d5de259820721ef043e152d0d8b15ef8e74
aknea001 <aknea001@osloskolen.no>
2025-03-09 00:19:20 +0100

    adda eButton tooltip which pops up when over door to dungeon

--- game/world/overworld/world1/dungeon_start.gd
+++ game/world/overworld/world1/dungeon_start.gd
@@ -2,17 +2,20 @@ extends Area2D
 
 var readyToGo = false
 
-@onready var enterLabel = $enterLabel
+@onready var toolTips = $toolTips
+
+func toolTipsState(enter: bool) -> void:
+	toolTips.visible = enter
 
 func _on_body_entered(_body: Node2D) -> void:
 	readyToGo = true
 
 func _on_body_exited(_body: Node2D) -> void:
 	readyToGo = false
-	enterLabel.visible = false
+	toolTipsState(false)
 
 func _process(_delta: float) -> void:
 	if readyToGo:
-		enterLabel.visible = true
+		toolTipsState(true)
 		if Input.is_action_just_pressed("enter"):
 			get_tree().change_scene_to_file("res://game/world/overworld/test_world/dungeonTypeShit.tscn")
