7dd39e6ae2324d3448c63175c95bdcccad53b05f
Aksel <aknea001@osloskolen.no>
2025-03-19 13:35:43 +0100

    fiksa at least levels n shit

--- next_level.gd
+++ next_level.gd
@@ -3,6 +3,6 @@ extends Area2D
 func _ready() -> void:
 	randomize()
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	var roomNum = randi() % 10
 	get_tree().change_scene_to_file.call_deferred("res://game/world/dungeon/rooms/room%s.tscn" % [str(roomNum)])
