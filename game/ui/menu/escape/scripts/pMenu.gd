41bb6ff25b531162e0a1fccd4fdcf2c35484e25d
Aksel <aknea001@osloskolen.no>
2025-03-19 13:45:41 +0100

    restarter hele shiten ved main menu

--- game/ui/menu/escape/scripts/pMenu.gd
+++ game/ui/menu/escape/scripts/pMenu.gd
@@ -5,8 +5,10 @@ func _on_resume_pressed() -> void:
 	get_tree().paused = false
 
 func _on_exit_pressed() -> void:
-	get_tree().paused = false
-	get_tree().change_scene_to_file("res://game/ui/menu/main_menu/main_menu.tscn")
+	#get_tree().paused = false
+	#get_tree().change_scene_to_file("res://game/ui/menu/main_menu/main_menu.tscn")
+	get_tree().quit()
+	OS.create_instance([])
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("menu"):
