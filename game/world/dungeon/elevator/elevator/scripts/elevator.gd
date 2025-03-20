358d7fe16f145baae80e15508ec7991e9753ab90
Oliver <olivik007@gmail.com>
2025-03-20 19:57:45 +0100

    Working elevator and better collisions

--- game/world/dungeon/elevator/elevator/scripts/elevator.gd
+++ game/world/dungeon/elevator/elevator/scripts/elevator.gd
@@ -1,12 +1,31 @@
 extends RigidBody2D
 
+@export var speed: float
+
 @onready var startTimer = $"../elevatorStarter"
 @onready var detector = $detector
 @onready var elevator = $"."
 
-func _on_detection(body: Node2D) -> void:
+@onready var leftDoor = $LeftDoor
+@onready var rightDoor = $RightDoor
+
+var elevatorMoving: bool = false
+
+func _on_detection(_body: Node2D) -> void:
 	startTimer.start()
 
 func _on_elevator_timer_timeout() -> void:
 	detector.monitoring = false
-	elevator.freeze = false
+	elevatorMoving = true
+	leftDoor.disabled = false
+	rightDoor.disabled = false
+	print("non-elevate")
+
+func _physics_process(delta: float) -> void:
+	if elevatorMoving:
+		if global_position.y >= 1720:
+			elevatorMoving = false
+			leftDoor.disabled = true
+			rightDoor.disabled = true
+		else:
+			global_position.y += speed * delta
