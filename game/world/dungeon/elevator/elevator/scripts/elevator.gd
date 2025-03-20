2e5295e935cec025b726f57a919b9a015d0033db
Oliver Vik <olivik007@gmail.com>
2025-03-20 10:34:18 +0100

    elevator prank
    elevator now falls

--- game/world/dungeon/elevator/elevator/scripts/elevator.gd
+++ game/world/dungeon/elevator/elevator/scripts/elevator.gd
@@ -1,1 +1,12 @@
 extends RigidBody2D
+
+@onready var startTimer = $"../elevatorStarter"
+@onready var detector = $detector
+@onready var elevator = $"."
+
+func _on_detection(body: Node2D) -> void:
+	startTimer.start()
+
+func _on_elevator_timer_timeout() -> void:
+	detector.monitoring = false
+	elevator.freeze = false
