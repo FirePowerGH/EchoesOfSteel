38999bdb38c53c518385c742b8ee0da0bde70f25
Oliver Vik <olivik007@gmail.com>
2025-04-02 14:28:21 +0200

    Save audio levels when exit game

--- common/autoload/music_controller.gd
+++ common/autoload/music_controller.gd
@@ -5,6 +5,9 @@ extends Node
 var exploration = load("res://common/music/Exploration.mp3")
 var titleMusic = load("res://common/music/FadingIntent.mp3")
 
+func _ready() -> void:
+	loadAudioSettings()
+
 func playExplore() -> void:
 	music.stream = exploration
 	music.play()
@@ -18,3 +21,12 @@ func stopMusic() -> void:
 
 func isPlaying():
 	return music.playing
+
+func loadAudioSettings() -> void:
+	var config = ConfigFile.new()
+	var err = config.load("user://audio_config.cfg")
+	if err == OK:
+		for i in range(AudioServer.get_bus_count()):
+			var busName = AudioServer.get_bus_name(i)
+			var volume = config.get_value("audioBusLevels", busName, 1.0)  # Default to full volume if not found
+			AudioServer.set_bus_volume_db(i, linear_to_db(volume))
--- game/ui/menu/main_menu/scripts/volume_slider.gd
+++ game/ui/menu/main_menu/scripts/volume_slider.gd
@@ -13,5 +13,22 @@ func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(busIndex, linear_to_db(value))
 
 func _on_drag_ended(value_changed: bool) -> void:
-	value = db_to_linear(AudioServer.get_bus_volume_db(busIndex))
-	print(value)
+	var config = ConfigFile.new()
+	var err = config.load("user://audio_config.cfg")
+
+	# Check if the file was loaded correctly or doesn't exist (create new)
+	if err != OK:
+		print("Config file not found, creating new one.")
+		
+	# Loop through all buses and save their values
+	for i in range(AudioServer.get_bus_count()):
+		var busName = AudioServer.get_bus_name(i)
+		var volume = db_to_linear(AudioServer.get_bus_volume_db(i))
+		config.set_value("audioBusLevels", busName, volume)
+		
+	# Save the updated config file
+	var save_err = config.save("user://audio_config.cfg")
+	if save_err != OK:
+		print("Error saving audio config: ", save_err)
+	else:
+		return
