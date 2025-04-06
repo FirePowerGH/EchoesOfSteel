fc3f486c9f6bfa5ca40e5c79bc718f725ed8a869
Oliver <olivik007@gmail.com>
2025-04-06 19:39:48 +0200

    made controls scene

---
+++ game/ui/menu/main_menu/controls/controls.gd
@@ -0,0 +1,25 @@
+extends Control
+
+func _ready() -> void:
+	update_keybind_label()
+
+func _process(_delta: float) -> void:
+	pass
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://game/ui/menu/main_menu/options.tscn")
+
+func get_action_key_text(action_name: String) -> String:
+	var events = InputMap.action_get_events(action_name)
+	for event in events:
+		if event is InputEventKey:
+			return OS.get_keycode_string(event.keycode)
+	return "Unbound"
+
+func update_keybind_label() -> void:	
+	for container in $CenterContainer/VBoxContainer/HBoxContainer.get_children():
+		for button in container.get_children():
+			if button is Button:
+				var action = button.name.to_lower()
+				var key = get_action_key_text(action)
+				button.text = action.to_upper() + ": " + key
--- game/ui/menu/main_menu/scripts/options.gd
+++ game/ui/menu/main_menu/scripts/options.gd
@@ -5,3 +5,6 @@ func _on_back_pressed() -> void:
 
 func _on_audio_pressed() -> void:
 	get_tree().change_scene_to_file("res://game/ui/menu/main_menu/audio/audio.tscn")
+
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://game/ui/menu/main_menu/controls/controls.tscn")
